3375e76172d7238c0bac9fd225ba8ce39c560ab3
Ahmad Salous <yaqot.1099@gmail.com>
2024-05-14 12:51:54 +0300

    do some editing on the structure trying to follow the perfect way of structuring

--- home.js
+++ home.js
@@ -1,7 +1,8 @@
 let burgerFlag = false;
 document.getElementById("burger-menu").addEventListener("click", () => {
   if (!burgerFlag) {
-    document.getElementById("mobile-menu-items").style.display = "flex";
+    document.getElementsByClassName("menu-items")[0].style.display = "flex";
+    document.getElementsByClassName("section-nav")[0].style.display = "flex";
     document.getElementsByTagName(
       "body"
     )[0].style.backgroundImage = `linear-gradient(to left, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))`;
@@ -16,7 +17,8 @@ document.getElementById("burger-menu").addEventListener("click", () => {
 
 document.getElementById("exit-burger-menu").addEventListener("click", () => {
   if (burgerFlag) {
-    document.getElementById("mobile-menu-items").style.display = "none";
+    document.getElementsByClassName("menu-items")[0].style.display = "none";
+    document.getElementsByClassName("section-nav")[0].style.display = "none";
     document.getElementsByTagName("body")[0].style.backgroundImage = "none";
     document.getElementsByClassName("home-article")[0].style.backgroundImage =
       "none";
